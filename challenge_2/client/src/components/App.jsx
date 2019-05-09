eec62bcae344bb0a507837835f75cae5bf7f638c
Harrison Wu <harrisonywu@gmail.com>
2019-05-09 12:07:14 -0700

    Send axios request when component mounts to the server

--- challenge_2/client/src/components/App.jsx
+++ challenge_2/client/src/components/App.jsx
@@ -1,6 +1,15 @@
 import React, { Component } from 'react';
-
+import axios from 'axios';
 class App extends Component {
+  constructor() {
+    super();
+  }
+
+  componentDidMount() {
+    axios.get('/api/coindesk')
+      .then((data) => console.log('data returned: ', data.data))
+  }
+ 
   render() {
     return(
       <div>
--- challenge_2/server/server.js
+++ challenge_2/server/server.js
@@ -1,11 +1,17 @@
 const express = require('express');
-const bodyparser = require('body-parser');
+const bodyParser = require('body-parser');
 const path = require('path');
 
 const app = express();
 const port = process.env.PORT | 3000;
 
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
 
 app.use(express.static(path.join(__dirname, '../public')));
+
+app.get('/api/coindesk', (req, res) => {
+  res.send('connected to componentDidMount')
+})
 
 app.listen(port, () => console.log(`Listening on port ${port}!`))
