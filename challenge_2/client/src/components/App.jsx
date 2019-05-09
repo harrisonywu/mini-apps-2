05812474f855ace3fe4bd8b330d651f834210edc
Harrison Wu <harrisonywu@gmail.com>
2019-05-09 11:42:07 -0700

    Setup html and components to load bundled React

---
+++ challenge_2/client/src/components/App.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return(
+      <div>
+        App rendering!
+      </div>
+    )
+  }
+}
+
+export default App;
