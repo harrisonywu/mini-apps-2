67999d2f4d3a96c8fcbc4d74ae9d6cd493a4db08
Harrison Wu <harrisonywu@gmail.com>
2019-05-10 11:51:13 -0700

    Implement buttons in Keypad

--- challenge_3/client/src/components/Keypad.jsx
+++ challenge_3/client/src/components/Keypad.jsx
@@ -1,9 +1,29 @@
 import React from 'react';
 
+// Buttons need to rerender OR just be unable to update score dependent on ...
+// ... if 2nd bowl of a frame, then only numbers that are 10 - 1st bowl will update score. Otherwise, an alert should appear saying
+// there aren't that many pins left!
+
 function Keypad(props) {
   return(
     <div>
-      Keypad app rendering!
+      <div>
+        <button>1</button>
+        <button>2</button>
+        <button>3</button>
+      </div>
+      <div>
+        <button>4</button>
+        <button>5</button>
+        <button>6</button>
+      </div>
+      <div>
+        <button>7</button>
+        <button>8</button>
+        <button>9</button>
+      </div>
+        <button>0</button>
+        <button>10</button>
     </div>
   )
 }
