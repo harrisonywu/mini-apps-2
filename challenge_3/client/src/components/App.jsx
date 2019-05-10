375dfeef4233b2e323d2e550a698561d5c8ed025
Harrison Wu <harrisonywu@gmail.com>
2019-05-10 11:36:33 -0700

    Create keypadd component connected to App

--- challenge_3/client/src/components/App.jsx
+++ challenge_3/client/src/components/App.jsx
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
-
+import Keypad from './Keypad.jsx'
 class App extends Component {
   render() {
     return (
-      <div>App rendering!</div>
+      <div>
+        <div>App rendering!</div>
+        <Keypad />
+      </div>
     )
   }
 }
---
+++ challenge_3/client/src/components/Keypad.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Keypad(props) {
+  return(
+    <div>
+      Keypad app rendering!
+    </div>
+  )
+}
+
+export default Keypad;
