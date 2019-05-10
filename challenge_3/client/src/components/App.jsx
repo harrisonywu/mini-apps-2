1f393c3c5b6d95f810a489ab1234addd83f5c658
Harrison Wu <harrisonywu@gmail.com>
2019-05-10 12:13:28 -0700

    Create function updating total score when numbered button is pressed

--- challenge_3/client/src/components/App.jsx
+++ challenge_3/client/src/components/App.jsx
@@ -1,11 +1,31 @@
 import React, { Component } from 'react';
 import Keypad from './Keypad.jsx'
 class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      totalScore: 0,
+      currentFrame: 1,
+    }
+  }
+
+  updateTotalScore(e) {
+    let pinsHit = parseFloat(e.target.textContent);
+    let newScore = this.state.totalScore += pinsHit;
+    this.setState({ totalScore: newScore })
+  }
+
   render() {
+
     return (
       <div>
         <div>App rendering!</div>
-        <Keypad />
+        <div>Total Pins Knocked: {this.state.totalScore}</div>
+        <Keypad 
+          updateTotalScore={this.updateTotalScore.bind(this)}
+        />
+
       </div>
     )
   }
