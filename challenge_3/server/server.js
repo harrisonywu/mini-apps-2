b28a5950cfeeecf907e60e082af75016930b6d5e
Harrison Wu <harrisonywu@gmail.com>
2019-05-10 11:29:19 -0700

    Create server hosting html at port 3000

--- challenge_3/server/server.js
+++ challenge_3/server/server.js
@@ -1,2 +1,11 @@
+const express = require('express');
+const path = require('path');
+const morgan = require('morgan');
+const app = express();
 
+const port = process.env.PORT || 3000;
 
+app.use(morgan('dev'));
+app.use(express.static(path.join(__dirname, '../public')));
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
